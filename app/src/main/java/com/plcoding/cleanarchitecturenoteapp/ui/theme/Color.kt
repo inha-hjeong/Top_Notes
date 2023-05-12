a8ea016e55b7d131c766171485673d32147781d9
abdul <abdullod01@gmail.com>
2023-05-13 00:22:54 +0500

    Initial Commit

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.plcoding.cleanarchitecturenoteapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xff374151)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0xffffab91)
+val RedPink = Color(0xfff48fb1)
+val BabyBlue = Color(0xff81deea)
+val Violet = Color(0xffcf94da)
+val LightGreen = Color(0xffe7ed9b)
+val LightGreen2 = Color(0xffd1fae5)
+val a = Color(0xff8b5cf6)
+val b = Color(0xff5eead4)
+val c = Color(0xff6ee7b7)
+
